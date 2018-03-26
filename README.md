250121ce1707b0bd85ee9daaa79fb1b8a1b54540
tangston <37773841+tangston@users.noreply.github.com>
2018-03-26 18:37:35 +0800

    创了一个按钮
    第一个版本，只能弹一个窗户和一个没卵用默认大小的按钮

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# my project
+﻿# my project
+#能输中文吗
---
+++ creatButton/src/creatButton/ClassButton.java
@@ -0,0 +1,39 @@
+﻿
+package creatButton;
+//import java package
+import java.awt.*;
+import javax.swing.*;
+/**
+ * @author ts
+ *
+ */
+public class ClassButton extends JFrame {
+	private static final int width=600,height=600;
+	/**
+	 * @param args
+	 */
+	/*需要的swing组件添加在了这里*/
+	private JFrame jf=null;
+	JButton jb1=null;
+	public static void main(String[] args) {
+		
+		ClassButton cb=new ClassButton();
+	 
+	}
+	public ClassButton()//构造函数
+	{
+		// TODO 自动生成的方法存根
+		//jFrame 是一个顶层容器，里面可以添加其他swing组建的类
+		this.jf=new JFrame();
+		this.jb1=new JButton("我是按钮");
+		//框架设置
+		jf.setTitle("tangstone's first window");
+		jf.setSize(width, height);		
+		jf.setLocation(100, 200);
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jf.setVisible(true);
+		//按键设置
+		jf.add(jb1);
+	}
+			
+}
