b87645ccc9afc9c0e3257aa9753c81f9cfe99003
tangston <37773841+tangston@users.noreply.github.com>
2018-04-11 20:39:17 +0800

    keyListener
    keyListener practice

---
+++ tankWar/src/eventTest/Demo.java
@@ -0,0 +1,54 @@
+package eventTest;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Demo extends JFrame {
+	MyPanel mp=null;
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		Demo demo=new Demo();
+		
+	}
+	
+	public Demo(){
+		mp=new MyPanel();
+		this.add(mp);
+		this.addKeyListener(mp);
+		this.setSize(300, 400);
+		this.setVisible(true);
+		this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+	}
+}
+
+class MyPanel extends JPanel implements KeyListener{
+	int x=10,y=10;
+	public void paint(Graphics g)
+	{
+		super.paint(g);
+		g.fillOval(x ,y, 10, 10);
+	}
+	//处理事件发生后的执行的方法
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO 自动生成的方法存根
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO 自动生成的方法存根
+		if(e.getKeyCode()==KeyEvent.VK_DOWN)
+		{
+			y++;
+			System.out.println("down pressed");
+		}
+		repaint();
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO 自动生成的方法存根
+		
+	}
+	
+}
--- tankWar/src/tankWar/TankGame.java
+++ tankWar/src/tankWar/TankGame.java
@@ -1,7 +1,7 @@
 package tankWar;
 import java.awt.*;
 import javax.swing.*;
-
+import java.io.*;
 public class TankGame extends JFrame
 {
 	MyPanel mp=null;
@@ -24,22 +24,51 @@ class MyPanel extends JPanel{
 	public MyPanel(){
 		hero=new Hero(10,10);
 	}
-	public void drawTank(int x,int y,Graphics g,int direct,int colorType){
+	public void drawTank(int x,int y,Graphics g,int direction,int colorType){
 		switch(colorType)//判断坦克的类型
 		{
 		case 0:g.setColor(Color.cyan);	break;
 		case 1:g.setColor(Color.green);	break;
 		}
-		//paint left rectango
-		g.fill3DRect(x,y,5,30,false);
-		//paint mid rectango
-		g.fill3DRect(x+5,y+5,10,20,false);
-		//paint right rectango
-		g.fill3DRect(x+15,y,5,30,false);
-		//paint circle
-		g.fillOval(x+5, y+10, 10, 10);
-		//paint line
-		g.drawLine(x+10,y+15,x+10,y+40);
+		
+		
+		switch(direction)
+		{
+		case 0://画出向上的模型
+			g.fill3DRect(x, y, 5, 30, false);
+			g.fill3DRect(x+15, y, 5, 30, false);
+			g.fill3DRect(x+5, y+5,10, 20, false);
+			g.fillOval(x+5, y+15, 10, 10);
+			g.drawLine(x+10,y+15,x+10,y);
+			break;
+			
+		case 1://画出向右的模型
+			g.fill3DRect(x, y,30 , 5, false);
+			g.fill3DRect(x, y+15,30 , 5, false);
+			g.fill3DRect(x+5, y+5,20, 10, false);
+			g.fillOval(x+10, y+5, 10, 10);
+			g.drawLine(x+15,y+10,x+30,y+10);
+			break;
+			
+			
+		case 2://画出向下的模型
+			g.fill3DRect(x, y,30 , 5, false);
+			g.fill3DRect(x, y+15,30 , 5, false);
+			g.fill3DRect(x+5, y+5,20, 10, false);
+			g.fillOval(x+10, y+5, 10, 10);
+			g.drawLine(x+10,y+15,x+10,y+30);
+			break;
+			
+			
+		case 3://画出向左的模型
+			g.fill3DRect(x, y,30 , 5, false);
+			g.fill3DRect(x, y+15,30 , 5, false);
+			g.fill3DRect(x+5, y+5,20, 10, false);
+			g.fillOval(x+10, y+5, 10, 10);
+			g.drawLine(x+15,y+10,x,y+10);
+			break;
+		}
+	
 	}
 	public void paint(Graphics g)//paint my tank
 	{
@@ -54,7 +83,35 @@ class Tank
 {
 	//tank's   coordinate
 	int x=0,y=0;
+	int direction=0;//0->3 is the up right down and left
+	boolean isLive=true;
+	int speed=1;
+	int color=0;//enemy tank = 1,hero =0
+	public int getDirection() {
+		return direction;
+	}
 
+	public void setDirection(int direction) {
+		this.direction = direction;
+	}
+
+	public void setColor(int color) {
+		this.color=color;
+	}
+
+	public  int getColor() {
+		return color;
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+	
 	public int getX() {
 		return x;
 	}
@@ -79,5 +136,6 @@ class Hero extends Tank
 	{
 		this.setX(x);
 		this.setY(y);
+		this.setColor(0);
 	}	
 }
